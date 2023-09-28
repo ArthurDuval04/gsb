9db6b50221520afd46ddea3df3d2ea8abb715961
Arthur <leroiarthurmalraux@gmail.com>
2023-09-28 11:17:05 +0200

    fnvrnjrtnjrtk

--- vues/v_creation.php
+++ vues/v_creation.php
@@ -29,7 +29,7 @@
 				<div class="box">
 						
           <div class="content-wrap">
-						<legend>je suis médecin, je souhaite créer un compte</legend>
+						<legend>Je suis médecin, je souhaite créer un compte</legend>
 							<form method="post" action="index.php?uc=creation&action=valideCreation">
                   <input name="login" class="form-control" type="email" placeholder="mail"/>
 							    <input name="mdp" class="form-control" type="password" placeholder="password"/>
